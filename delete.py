e25e88540b540f655a8ee89cb00c6111684e54d8
hashiket <hashiketbhoyar@gmail.com>
2023-05-07 16:15:27 +0530

    Adding files

--- delete.py
+++ delete.py
@@ -1,14 +1,28 @@
-def mergeSort(arr):
-    if len(arr)>1:
-        mid = len(arr)//2
+def partition(arr, s, e):
+    pivot_index = s
+    pivot = arr[pivot_index]
 
-        left_arr = arr[:mid]
-        right_arr = arr[mid:]
+    while s<e:
+        while s<len(arr) and arr[s]<=pivot:
+            s+=1
 
-        mergeSort(left_arr)
-        mergeSort(right_arr)
+        while arr[e]>pivot:
+            e-=1
 
-        i=j=k=0
-        for i<len(left_arr) and j<len(right_arr):
-            if left_arr[i] < right_arr[j]:
-                arr[]
+        if s<e:
+            arr[s],arr[e]=arr[e],arr[s]
+
+    arr[pivot_index],arr[e]=arr[e],arr[pivot_index]
+    return e
+
+
+def quickSort(arr, s, e):
+    if s<e:
+        p = partition(arr, s, e)
+        quickSort(arr, s, p-1)
+        quickSort(arr, p+1, e)
+
+arr = [90,89,7,56,34,2,32,10]
+
+quickSort(arr, 0, len(arr)-1)
+print(arr)
